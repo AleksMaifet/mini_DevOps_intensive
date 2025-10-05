27aa8c114cb89a533d533262a05b6973de9b2ffa
creative <aleksmaifet@gmail.com>
2025-10-05 22:22:08 +0300

    feat: ci/cd changes back

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,4 @@
+import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -25,6 +26,7 @@ export default defineConfig([
             'no-unused-vars': ['error', {varsIgnorePattern: '^[A-Z_]'}],
         },
     },
+    // Новый блок для тестов
     {
         files: ['**/*.test.{js,jsx}'],
         languageOptions: {
